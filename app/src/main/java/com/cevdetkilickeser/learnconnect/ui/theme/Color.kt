0442ec29d76c3a6c383177916e94b57e85a4d14a
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-12-02 22:18:44 +0300

    set theme and colors

--- app/src/main/java/com/cevdetkilickeser/learnconnect/ui/theme/Color.kt
+++ app/src/main/java/com/cevdetkilickeser/learnconnect/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.cevdetkilickeser.learnconnect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val SharedPrimary = Color(0xFF6360FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val DarkBackground = Color(0xFF161719)
+val DarkSurface = Color(0xFF8F8E94)
+val DarkText = Color.White
+
+
+val LightBackground = Color(0xFFF1F1FA)
+val LightSurface = Color(0xFFFFFFFF)
+val LightTextPrimary = Color.Black
+val LightTextOnPrimary = Color.White
--- app/src/main/java/com/cevdetkilickeser/learnconnect/ui/theme/Theme.kt
+++ app/src/main/java/com/cevdetkilickeser/learnconnect/ui/theme/Theme.kt
@@ -1,51 +1,39 @@
 package com.cevdetkilickeser.learnconnect.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+val DarkColorScheme = darkColorScheme(
+    primary = SharedPrimary,
+    onPrimary = DarkText,
+    background = DarkBackground,
+    onBackground = DarkText,
+    surface = DarkSurface,
+    onSurface = DarkText,
+    outline = SharedPrimary
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val LightColorScheme = lightColorScheme(
+    primary = SharedPrimary,
+    onPrimary = LightTextOnPrimary,
+    background = LightBackground,
+    onBackground = LightTextPrimary,
+    surface = LightSurface,
+    onSurface = LightTextPrimary,
+    outline = SharedPrimary
 )
+
 
 @Composable
 fun LearnConnectTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
+        isDarkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
